391e203a17703443052a0b9ae34435de5a4a9b08
Piotr <piotr.ferst@o2.pl>
2022-05-16 00:30:02 +0200

    Basics finished

---
+++ API/BingoAPI/Game.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BingoAPI
+{
+    public class Game
+    {
+        public int Id { get; set; }
+
+        [StringLength(10)]
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ API/BingoAPI/PlayerText.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BingoAPI
+{
+    [Keyless]
+    public class PlayerText
+    {
+        public int TextId { get; set; }
+        public Text? Text { get; set; }
+        public int PlayerId { get; set; }
+        public Player? Player { get; set; }
+    }
+}
